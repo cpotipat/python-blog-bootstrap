b494953f9e550fd84f3fa1fd780ba9e67543d492
cpotipat <namsom.chitchanok@gmail.com>
2021-05-16 15:00:49 -0700

    Rendering individual posts and fix some minor bugs

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
+import requests
+
+all_posts = requests.get("https://api.npoint.io/0067e63917ca7a5034d9").json()
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def home():
-    return render_template("index.html")
+def get_all_post():
+    return render_template("index.html", posts=all_posts)
 
 
 @app.route("/about")
@@ -16,6 +20,15 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for post in all_posts:
+        if post["id"] == index:
+            requested_post = post
+    return render_template("post.html", post=requested_post)
 
 
 if __name__ == "__main__":
